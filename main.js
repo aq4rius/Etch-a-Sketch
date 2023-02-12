5a69f1646a1e3d6c959cf1adf2e7c72492fc7705
Aq4rius <maksbielecki6@gmail.com>
2023-02-13 00:08:45 +0100

    Add grid, drawing functionality and changing the size of the squares using prompt

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 const container = document.querySelector(".container");
+let squareNumberPerSide = prompt("How many squares per side?");
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+let containerArea = 320 * 320;
+
+let squareSize = Math.sqrt(containerArea / Math.pow(squareNumberPerSide, 2));
+container.style.gridTemplateColumns = `repeat(${squareNumberPerSide},1fr)`;
+
+drawGrid(squareNumberPerSide);
+
+function drawGrid(squareNumberPerSide) {
+  let squareSize = Math.sqrt(containerArea / Math.pow(squareNumberPerSide, 2));
+  container.style.gridTemplateColumns = `repeat(${squareNumberPerSide},1fr)`;
+  for (let i = 0; i < squareNumberPerSide * squareNumberPerSide; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+    square.style.border = "1px solid";
+    container.appendChild(square);
+  }
 }
 
-const squareDiv = document.querySelectorAll("div.square");
-let squareArray = Array.from(squareDiv);
+const squares = document.querySelectorAll("div.square");
+let squaresArray = Array.from(squares);
 
-window.addEventListener("mouseover", (e) => {
-  for (let i = 0; i < squareArray.length; i++) {
-    if (e.target === squareArray[i]) {
-      squareArray[i].style.backgroundColor = "blue";
+document.addEventListener("mousedown", () => {
+  isDrawing = true;
+  document.addEventListener("mouseover", drawFunction);
+});
+
+document.addEventListener("mouseup", () => {
+  isDrawing = false;
+  document.removeEventListener("mouseover", drawFunction);
+});
+
+function drawFunction(e) {
+  if (isDrawing) {
+    for (let i = 0; i < squaresArray.length; i++) {
+      if (e.target === squaresArray[i]) {
+        squaresArray[i].style.backgroundColor = "black";
+      }
     }
   }
-});
+}
